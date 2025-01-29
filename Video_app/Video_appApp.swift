e2a1d9fff3a540e1b7672a56015f83c5ee644de4
Afrah-saleh <afrahsaleh49@hotmail.com>
2025-01-29 23:20:37 +0300

    Initial Commit

---
+++ Video_app/Video_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Video_appApp.swift
+//  Video_app
+//
+//  Created by Afrah Saleh on 29/07/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct Video_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
